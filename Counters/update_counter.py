10b590806aa02cb2eb9400927c7a19ddc1ea62c4
mujahed.mca <mujahed.mca@gmail.com>
2024-06-28 22:46:42 +0530

    update_counter yaml file with PAT

---
+++ Counters/update_counter.py
@@ -0,0 +1,19 @@
+import os
+
+counter_file = 'counter.txt'
+
+# Read the current counter value
+if os.path.exists(counter_file):
+    with open(counter_file, 'r') as file:
+        counter = int(file.read().strip())
+else:
+    counter = 0
+
+# Increment the counter
+counter += 1
+
+# Write the new counter value back to the file
+with open(counter_file, 'w') as file:
+    file.write(str(counter))
+
+print(f"::set-output name=counter::{counter}")
